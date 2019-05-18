e676c4ebc1df398552e4aa49c6a207b2f1b7b749
ChienTeLee <42949820+ChienTeLee@users.noreply.github.com>
2019-05-18 09:21:48 -0400

    Add files via upload

--- download_pascal_dataset.sh
+++ download_pascal_dataset.sh
@@ -1,4 +1,5 @@
-!mkdir -p dataset
-!!wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
-!tar -xvf VOCtrainval_11-May-2012.tar -C ./dataset
-!rm VOCtrainval_11-May-2012.tar
+pip install matplotlib pillow tensorboardX tqdm
+mkdir -p dataset
+!wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+tar -xvf VOCtrainval_11-May-2012.tar -C ./dataset
+rm VOCtrainval_11-May-2012.tar
