ec4b2ae711f9c8675c353a592d695f0b959b6f34
ChienTeLee <42949820+ChienTeLee@users.noreply.github.com>
2019-05-18 09:19:15 -0400

    Add files via upload

---
+++ download_pascal_dataset.sh
@@ -0,0 +1,4 @@
+!mkdir -p dataset
+!!wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+!tar -xvf VOCtrainval_11-May-2012.tar -C ./dataset
+!rm VOCtrainval_11-May-2012.tar
